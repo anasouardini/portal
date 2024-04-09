512a46ecf3bc4b760481bfd8657d2064f92373a8
anasouardini <ouardinianas@proton.me>
2024-04-09 22:23:04 +0000

    fix: adhering to convention

--- portal.plugin.zsh
+++ portal.plugin.zsh
@@ -1,23 +1,23 @@
 #!/bin/env sh
 
-pluginPath="$HOME/.local/share/zsh/plugins/portal/portal.plugin.zsh";
+# pluginPath="$HOME/.local/share/zsh/plugins/portal/portal.plugin.zsh";
 
-# set aliases
-function pc(){
-  bash $pluginPath create $1 $(pwd);
-}
-function pj(){
-  eval $(bash $pluginPath jump $1);
-}
-function pd(){
-  bash $pluginPath $1;
-}
-function pl(){
-  bash $pluginPath list $1;
-}
-function ph(){
-  bash $pluginPath help;
-}
+# # set aliases
+# function pc(){
+#   bash $pluginPath create $1 $(pwd);
+# }
+# function pj(){
+#   eval $(bash $pluginPath jump $1);
+# }
+# function pd(){
+#   bash $pluginPath $1;
+# }
+# function pl(){
+#   bash $pluginPath list $1;
+# }
+# function ph(){
+#   bash $pluginPath help;
+# }
 
 # portalScriptPath="$(pwd)/${BASH_SOURCE[0]}";
 # portalScriptPath="$HOME/.shconf/rc/tools/portal";
@@ -36,7 +36,7 @@ fi
 ## source portals
 source $portalScriptPath;
 
-function portalJump(){
+function _portalJump(){
   if [ -z $1 ]; then
     echo "echo 'provide a portal name.'";
     exit 1;
@@ -49,16 +49,16 @@ function portalJump(){
   echo "cd $path";
 }
 
-function portalCreate(){
+function _portalCreate(){
   # sed -i '0,/-- ports --/{s|-- ports --|-- ports --\nports\['\""$1"\"'\]='"\"$2\""';|}' $portalScriptPath;
   echo "ports[\"$1\"]=\"$2\"" >> $portalScriptPath;
 }
 
-function portalDelete(){
+function _portalDelete(){
   sed -i '/^ports\[\"'"$1"'\"\]/d' $portalScriptPath;
 }
 
-function portalList(){
+function _portalList(){
   if [[ -z $1 ]]; then
     for key in "${!ports[@]}"; do
       echo -e "\e[33m$key\e[0m ${ports[$key]}";
@@ -73,14 +73,17 @@ function portalList(){
   fi
 }
 
-if [[ $1 == "create" ]]; then
-  portalCreate $2 $3;
-elif [[ $1 == "jump" ]]; then
-  portalJump $2
-elif [[ $1 == "delete" ]]; then
-  portalDelete $2
-elif [[ $1 == "list" ]]; then
-  portalList $2
-elif [[ $1 == "help" ]]; then
-  echo "options: create (pc) | jump (pj) | delete (pd) | list (pl) | help (ph)";
-fi
+
+function portal(){
+  if [[ $1 == "create" ]]; then
+    _portalCreate $2 $3;
+  elif [[ $1 == "jump" ]]; then
+    _portalJump $2
+  elif [[ $1 == "delete" ]]; then
+    _portalDelete $2
+  elif [[ $1 == "list" ]]; then
+    _portalList $2
+  elif [[ $1 == "help" ]]; then
+    echo "options: create (pc) | jump (pj) | delete (pd) | list (pl) | help (ph)";
+  fi
+}
